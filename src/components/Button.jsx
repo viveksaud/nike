5f681e56230b0df4d532a216b683d18c0ba5d0f8
viveksaud <viveksaud3@gmail.com>
2024-10-03 20:38:00 +0545

    offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,28 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 border px-7 py-4 font-montserrat text-lg leading-none 
+    ${
+      backgroundColor
+        ? `${backgroundColor} ${textColor} ${borderColor}`
+        : "bg-coral-red  text-white border-coral-red"
+    } 
+    rounded-full`}
+    >
       {label}
-      {iconUrl && <img
-        src={iconUrl}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />}
+      {iconUrl && (
+        <img
+          src={iconUrl}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
       {/* if there is icon url then only show the image */}
     </button>
   );
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Button } from "../components";
+import { offer } from "../assets/images";
+import { arrowRight } from "../assets/icons";
 
-const SpecialOffer = () => {
+const SuperQuality = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className="flex justify-between items-center w-full gap-10 max-container max-xl:flex-col-reverse">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="Shoe Promotion"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
 
-export default SpecialOffer
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-bold font-palanquin lg:max-w-lg">
+          <span className="text-coral-red"> Special</span> Offer
+        </h2>
+
+        <p className="mt-6 lg:max-w-lg font-montserrat text-slate-gray text-lg leading-7">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg font-montserrat text-slate-gray text-lg leading-7">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+
+        <div className="mt-11 flex flex-wrap-reverse gap-4">
+          <Button label="Shop now" iconUrl={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
