cfce5d6b76e7602d4832b2c4b8f050cc1b16a6ae
viveksaud <viveksaud3@gmail.com>
2024-10-03 13:34:42 +0545

    popular product page

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Nav from "./Nav"
+import Button from "./Button"
+import ShoeCard from "./ShoeCard"
+import PopularProductCard from "./PopularProductCard"
+
+export {
+    Nav,
+    Button,
+    ShoeCard,
+    PopularProductCard
+}
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import PopularProductCard from "../components/PopularProductCard";
+import { products } from "../constants";
 
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
-  )
-}
+    <section id="products" className="max-container max-sm:mt-12">
+      <div>
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="text-slate-gray font-montserrat mt-2 lg:max-w-lg">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value
+        </p>
+      </div>
 
-export default PopularProducts
+      <div className="mt-16 grid grid-cols-1 gap-14 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-6">
+        {products.map((product, index) => (
+          <PopularProductCard
+            key={product.name}
+            imgUrl={product.imgURL}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PopularProducts;
