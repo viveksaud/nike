0de828241ced59ee4fe5513e5a4c7eff9656d6af
viveksaud <viveksaud3@gmail.com>
2024-10-03 16:03:24 +0545

    super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,12 @@ const Button = ({ label, iconUrl }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img
+      {iconUrl && <img
         src={iconUrl}
         alt="arrow right icon"
         className="ml-2 rounded-full w-5 h-5"
-      />
+      />}
+      {/* if there is icon url then only show the image */}
     </button>
   );
 };
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,7 +1,47 @@
+import { Button } from "../components";
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center w-full gap-10 max-container max-lg:flex-col"
+    >
 
-export default SuperQuality
+      <div className="flex flex-1 flex-col">
+      
+        <h2 className="text-4xl font-bold capitalize font-palanquin lg:max-w-lg">
+          We Provide You <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red">Quality </span> Shoes
+        </h2>
+
+        <p className="mt-6 lg:max-w-lg font-montserrat text-slate-gray text-lg leading-7">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+
+        <p className="mt-6 lg:max-w-lg font-montserrat text-slate-gray text-lg leading-7">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="product detail"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
